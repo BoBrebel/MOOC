7894aac6d5ff097cfddab2e90da752fbf2464e16
Nournourf <nour.faroua@esprit.tn>
2016-02-08 13:21:49 +0100

    commit

--- Tesseract/src/main/java/com/esprit/pidev/models/daos/interfaces/IObjectifDAO.java
+++ Tesseract/src/main/java/com/esprit/pidev/models/daos/interfaces/IObjectifDAO.java
@@ -5,10 +5,26 @@
  */
 package com.esprit.pidev.models.daos.interfaces;
 
+import com.esprit.tesseract.models.beans.Objectif;
+import java.sql.SQLException;
+import java.util.List;
+
 /**
  *
- * @author Choukou_Tracker
+ * @author Noor
  */
 public interface IObjectifDAO {
-    
+
+    boolean createObjectif(Objectif objectif) throws SQLException;
+
+    boolean removeObjectif(Objectif objectif) throws SQLException;
+
+    boolean removeObjectifByname(String nomObjectif) throws SQLException;
+
+    boolean updateObjectif(String nomObjectif, Objectif objectif) throws SQLException;
+
+    List<Objectif> displayObjectifs() throws SQLException;
+
+    Objectif getObjectifByName(String nomObjectif) throws SQLException;
+
 }
