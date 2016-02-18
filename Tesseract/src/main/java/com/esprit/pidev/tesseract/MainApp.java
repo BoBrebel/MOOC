53a5dfbad791c5f66aaffeda9b2a8c8e09331929
Mohamed el fadhel el hachemi <mohamedelfadhel.elhachemi@esprit.tn>
2016-02-18 13:03:35 +0100

    login scene undecorated

--- Tesseract/src/main/java/com/esprit/pidev/tesseract/MainApp.java
+++ Tesseract/src/main/java/com/esprit/pidev/tesseract/MainApp.java
@@ -13,6 +13,7 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 
 public class MainApp extends Application {
@@ -25,6 +26,7 @@ public class MainApp extends Application {
         Scene scene = new Scene(root);
 
         stage.setScene(scene);
+        stage.initStyle(StageStyle.UNDECORATED);
         stage.show();
     }
 
